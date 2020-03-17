642202c4da3c751ebf7f338e05cee16fb17baf79
Elias Zain <ezain2t@gmail.com>
2020-03-17 18:16:06 +0100

    add delta values for cases and deaths

--- update-data.js
+++ update-data.js
@@ -6,6 +6,18 @@ let date = process.argv[2];
 let dataPath = path.join(__dirname, 'data', date);
 let dailyReport = fs.readFileSync(dataPath, 'utf-8');
 
+const fileData = require('./datos-situacion');
+
+const getYesterdayData = () => {
+  const yesterday = process.argv[3];
+  if (!yesterday) return;
+
+  return fileData.filter(item => item.date === yesterday).reduce((data, item) => ({...data, [item.code]: item}), {});
+
+};
+
+const yesterdayData = getYesterdayData();
+
 const codes = [
   "ES.AN",
   "ES.AR",
@@ -36,20 +48,29 @@ let dailyData = dailyReport.split('\n').map((elem, index) => {
     date,
     name,
     cases: parseInt(cases),
+    deltaCases: !yesterdayData ? undefined : parseInt(cases) - yesterdayData[codes[index]].cases,
     deaths: parseInt(deaths),
+    deltaDeaths: !yesterdayData ? undefined : parseInt(deaths) - yesterdayData[codes[index]].deaths,
     code: codes[index]
   }
 });
 
-const total = dailyData.reduce((global, item) => ({
+let total = dailyData.reduce((global, item) => ({
   ...global,
   cases: global.cases + item.cases,
   deaths: global.deaths + item.deaths
 }), {date, name: 'España', code: 'ES', cases: 0, deaths: 0});
+
+if (yesterdayData) {
+  total = {
+    ...total,
+    deltaCases: total.cases - yesterdayData[total.code].cases,
+    deltaDeaths: total.deaths - yesterdayData[total.code].deaths
+  }
+}
 dailyData.push(total);
 
-
+console.log(dailyData)
 let outpath = path.join(__dirname, 'datos-situacion.json');
-const fileData = JSON.parse(fs.readFileSync(outpath));
 fileData.push(...dailyData);
 fs.writeFileSync(outpath, JSON.stringify(fileData, null, 2));
