5e554a741c74a7d73f4cc6bdc0d831c445200061
Elias Zain <ezain2t@gmail.com>
2020-03-21 13:31:21 +0100

    do not calculate delta

--- update-data.js
+++ update-data.js
@@ -8,16 +8,6 @@ let dailyReport = fs.readFileSync(dataPath, 'utf-8');
 
 const fileData = require('./datos-situacion');
 
-const getYesterdayData = () => {
-  const yesterday = process.argv[3];
-  if (!yesterday) return;
-
-  return fileData.filter(item => item.date === yesterday).reduce((data, item) => ({...data, [item.code]: item}), {});
-
-};
-
-const yesterdayData = getYesterdayData();
-
 const codes = [
   "ES.AN",
   "ES.AR",
@@ -48,9 +38,7 @@ let dailyData = dailyReport.split('\n').map((elem, index) => {
     date,
     name,
     cases: parseInt(cases),
-    deltaCases: !yesterdayData ? undefined : parseInt(cases) - yesterdayData[codes[index]].cases,
     deaths: parseInt(deaths),
-    deltaDeaths: !yesterdayData ? undefined : parseInt(deaths) - yesterdayData[codes[index]].deaths,
     code: codes[index]
   }
 });
@@ -61,13 +49,6 @@ let total = dailyData.reduce((global, item) => ({
   deaths: global.deaths + item.deaths
 }), {date, name: 'España', code: 'ES', cases: 0, deaths: 0});
 
-if (yesterdayData) {
-  total = {
-    ...total,
-    deltaCases: total.cases - yesterdayData[total.code].cases,
-    deltaDeaths: total.deaths - yesterdayData[total.code].deaths
-  }
-}
 dailyData.push(total);
 
 console.log(dailyData)
